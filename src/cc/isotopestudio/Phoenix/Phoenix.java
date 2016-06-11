5c6e4df4827a712d63b65d20ac207104c7d36f4c
Mars Tan <davidtjx424@live.com>
2016-06-11 22:00:52 +0800

    Add radius config

--- src/cc/isotopestudio/Phoenix/Phoenix.java
+++ src/cc/isotopestudio/Phoenix/Phoenix.java
@@ -2,14 +2,17 @@ package cc.isotopestudio.Phoenix;
 
 import com.earth2me.essentials.utils.LocationUtil;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,10 +22,18 @@ import java.util.Map;
  */
 public class Phoenix extends JavaPlugin implements Listener {
     private static final String pluginName = "Phoenix";
-    private static Map<Player, Location> death = new HashMap<>();
+    private static final Map<Player, Location> death = new HashMap<>();
+    private static int radius;
 
     @Override
     public void onEnable() {
+
+        File file;
+        file = new File(getDataFolder(), "config.yml");
+        if (!file.exists()) {
+            saveDefaultConfig();
+        }
+        radius = getConfig().getInt("radius", 0);
         getServer().getPluginManager().registerEvents(this, this);
         getLogger().info(pluginName + "成功加载!");
         getLogger().info(pluginName + "由ISOTOPE Studio制作!");
@@ -33,17 +44,33 @@ public class Phoenix extends JavaPlugin implements Listener {
     public void onPlayerDeath(PlayerDeathEvent event) {
         death.put(event.getEntity(), event.getEntity().getLocation());
     }
-
+/*
     @EventHandler
+    public void onPlayerRespawn(PlayerTeleportEvent event) {
+        for (RegisteredListener listener : event.getHandlers().getRegisteredListeners())
+            System.out.print(listener.getPlugin().getName());
+        System.out.print(event.getTo().toString() + " " + event.getCause());
+    }
+*/
+
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerRespawn(final PlayerRespawnEvent event) {
         final Location location = death.get(event.getPlayer());
         if (location == null) return;
+        location.add(Math.random() * radius / 2, 0, Math.random() * radius / 2);
         new BukkitRunnable() {
             @Override
             public void run() {
                 Location safe = null;
                 try {
                     safe = LocationUtil.getSafeDestination(location);
+                    if (safe.getWorld().getEnvironment() == World.Environment.NETHER && safe.getBlockY() >= 128) {
+                        safe = safe.add(0, -20, 0);
+                        int y = safe.getBlockY();
+                        while (safe.getBlockY() >= 128) {
+                            safe = LocationUtil.getSafeDestination(safe.add(Math.random() * 5, y - 5, Math.random() * 5));
+                        }
+                    }
                 } catch (Exception ignored) {
                     safe = location;
                 } finally {
@@ -51,6 +78,6 @@ public class Phoenix extends JavaPlugin implements Listener {
                     event.getPlayer().sendMessage("§c§l浴火重生");
                 }
             }
-        }.runTaskLater(this, 1);
+        }.runTaskLater(this, 2);
     }
 }
